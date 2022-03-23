6be33fc7f4403a2e58420f54d526becbff6021dd
Nate Volker <natevolker@gmail.com>
2022-03-23 07:34:34 -0500

    adding base url based on env

--- src/services/nextrip/index.ts
+++ src/services/nextrip/index.ts
@@ -1,21 +1,24 @@
 import { fetch } from '@utils/fetch'
 import { Route, Direction, Place, NexTripResult, Api as NexTrip } from './generated'
 
+const BASE_URL = process.env['NEXT_PUBLIC_VERCEL_URL'] ?? 'localhost:3000'
+const PROTOCOL = BASE_URL === 'localhost:3000' ? 'http://' : 'https://'
+
 export const getRoutes = async () =>
-  fetch<Route[]>(`http://localhost:3000/api/routes`)
+  fetch<Route[]>(`${PROTOCOL}${BASE_URL}/api/routes`)
 
 export const getDirections = async (route_id: string) =>
-  fetch<Direction[]>(`http://localhost:3000/api/directions?route_id=${route_id}`)
+  fetch<Direction[]>(`${PROTOCOL}${BASE_URL}/api/directions?route_id=${route_id}`)
 
 export const getStops = async (route_id: string, direction_id: string | number) =>
-  fetch<Place[]>(`http://localhost:3000/api/stops?route_id=${route_id}&direction_id=${direction_id}`)
+  fetch<Place[]>(`${PROTOCOL}${BASE_URL}/api/stops?route_id=${route_id}&direction_id=${direction_id}`)
 
 export const getDepartures = async (
   route_id: string,
   direction_id: string | number,
   stop_id: string
 ) => fetch<NexTripResult>(
-  `http://localhost:3000/api/departures?route_id=${route_id}&direction_id=${direction_id}&stop_id=${stop_id}`
+  `${PROTOCOL}${BASE_URL}/api/departures?route_id=${route_id}&direction_id=${direction_id}&stop_id=${stop_id}`
 )
 
 export const { nextripv2: nexTrip } = new NexTrip({
